c8a6d73265d1d91857677bd81bf5073d2fb68e4b
th7nder <vesphy@gmail.com>
2018-09-13 10:18:45 +0200

    feat: handle adding transactions

--- src/components/AddItem.jsx
+++ src/components/AddItem.jsx
@@ -1,25 +1,73 @@
-import React from "react";
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 
-const AddItem = () => (
-  <form onSubmit={e => e.preventDefault()}>
-    {[
-      { name: "date", desc: "Data" },
-      { name: "company", desc: "Firma" },
-      { name: "dimensions", desc: "Wymiary" },
-      { name: "amount", desc: "Ilość" },
-      { name: "grossMargin", desc: "Marża" },
-      { name: "revenue", desc: "Zysk" },
-      { name: "sum", desc: "Suma" }
-    ].map(item => (
-      <p>
-        <label htmlFor={item.name}>
-          {item.desc}
-          <input type={item.name} name={item.name} id={item.name} />
-        </label>
-      </p>
-    ))}
-    <button type="submit">Dodaj transakcje</button>
-  </form>
-);
+const formFields = [
+  { name: "date", desc: "Data" },
+  { name: "company", desc: "Firma" },
+  { name: "dimensions", desc: "Wymiary" },
+  { name: "amount", desc: "Ilość" },
+  { name: "grossMargin", desc: "Marża" },
+  { name: "revenue", desc: "Zysk" },
+  { name: "sum", desc: "Suma" }
+];
+
+const initialState = {
+  date: "",
+  company: "",
+  dimensions: "",
+  amount: "",
+  grossMargin: "",
+  revenue: "",
+  sum: ""
+};
+
+class AddItem extends Component {
+  constructor(props) {
+    super(props);
+    this.state = initialState;
+  }
+
+  handleChange(value, prop) {
+    this.setState({ [prop]: value });
+  }
+
+  render() {
+    const { onAddItem } = this.props;
+    return (
+      <form
+        onSubmit={e => {
+          e.preventDefault();
+          onAddItem(this.state);
+          this.setState(initialState);
+        }}
+      >
+        {formFields.map(item => {
+          const { [item.name]: value } = this.state;
+
+          return (
+            <p>
+              <label htmlFor={item.name}>
+                {item.desc}
+                <input
+                  type={item.name}
+                  name={item.name}
+                  id={item.name}
+                  onChange={e => {
+                    this.handleChange(e.target.value, item.name);
+                  }}
+                  value={value}
+                />
+              </label>
+            </p>
+          );
+        })}
+        <button type="submit">Dodaj transakcje</button>
+      </form>
+    );
+  }
+}
+AddItem.propTypes = {
+  onAddItem: PropTypes.func.isRequired
+};
 
 export default AddItem;
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,16 +2,35 @@ import React from "react";
 import Table from "./Table";
 import AddItem from "./AddItem";
 
-const App = () => (
-  <div>
-    <h1>
-      <span>Foxpak</span>
-    </h1>
-    <main>
-      <AddItem />
-      <Table />
-    </main>
-  </div>
-);
+const initialState = {
+  transactions: []
+};
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = initialState;
+  }
+
+  handleAddItem = itemData => {
+    this.setState(previousState => ({
+      transactions: [...previousState.transactions, itemData]
+    }));
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>
+          <span>Foxpak</span>
+        </h1>
+        <main>
+          <AddItem onAddItem={this.handleAddItem} />
+          <Table />
+        </main>
+      </div>
+    );
+  }
+}
 
 export default App;
